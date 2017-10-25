6d4d5556fdbe87d8a0cf15cbd1469b17bd8518f8
KhaledKhussein <mister.nuva@gmail.com>
2017-10-25 01:50:08 -0400

    First Commit
    Added to Master

---
+++ CarWithCollision/Assets/Standard Assets/collectObjects.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+function Start () {
+
+score = 0;
+
+	
+}
+
+function Update () {
+	
+}
+
+
+var score:int;
+
+
+function OnControllerColliderHit(hit : ControllerColliderHit)
+{
+var label:String = hit.collider.gameObject.tag;
+
+	if(hit.collider.tag == "pick_me")
+	{
+	score = score + 1;
+	print("Score:"+score);
+		print("Collided with"+ label);
+		Destroy(hit.collider.gameObject);
+		if(score >= 4) Application.LoadLevel("Scene2");
+	}
+
+
+}
